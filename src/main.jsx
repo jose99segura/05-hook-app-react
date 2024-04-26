d4838cae47cfb04c0a5b419e9aeab9e3322369ac
jose99segura <jose99segura@gmail.com>
2024-04-26 14:58:56 +0200

    useRef and useLayoutEffect and useMemo

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,12 @@ import './index.css'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook.jsx'
 // import { SimpleForm } from './02-useEffect/SimpleForm.jsx'
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook.jsx'
-import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks.jsx'
+// import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks.jsx'
+// import { FocusScreen } from './04-useRef/FocusScreen.jsx'
+// import { Layout } from './05-useLayoutEffect/Layout.jsx'
+import { Memorize } from './06-memos/Memorize.jsx'
+import { MemoHook } from './06-memos/MemoHook.jsx'
+import { CallbackHook } from './06-memos/CallbackHook.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -15,6 +20,12 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     {/* <CounterWithCustomHook></CounterWithCustomHook> */}
     {/* <SimpleForm></SimpleForm> */}
     {/* < FormWithCustomHook/> */}
-    < MultipleCustomHooks/>
+    {/* < MultipleCustomHooks/> */}
+    {/* <FocusScreen /> */}
+    {/* <Layout /> */}
+    {/* <Memorize /> */}
+    {/* <MemoHook /> */}
+    <CallbackHook />
+
   </React.StrictMode>,
 )
